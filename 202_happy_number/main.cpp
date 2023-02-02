e4ed1e6e289eaf10855050369e96b823e5e611cc
Ervin <75392265+FrequentErvin@users.noreply.github.com>
2023-02-02 18:35:30 +0100

    Solve 3 and 202

---
+++ 202_happy_number/main.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+class Solution {
+public:
+    bool isHappy(int n) {
+        long long num = 0;
+        int counter = 0;
+        while(counter < 20){
+            num = 0;
+            while(n > 0){
+            //cout << "NUM " << num << endl;
+            num += (n % 10) * (n % 10);
+            n /= 10;
+            }
+            cout << num << endl;
+            if(num == 1) return true;
+            n = num;
+            counter++;
+        }
+    }
+};
+int main()
+{
+    int n = 5;
+    Solution sol;
+    cout << sol.isHappy(n);
+}
--- 3_longest_substring_withouth_repeating_characters/main.cpp
+++ 3_longest_substring_withouth_repeating_characters/main.cpp
@@ -6,23 +6,30 @@ public:
     int lengthOfLongestSubstring(string s) {
         int l = 0;
         int r = 0;
-        int maxLength = 1;
+        int maxLength = 0;
         int currLength = 0;
         unordered_map<int, int> tempHash;
         while(r != s.size()){
-            if(tempHash.count(s[r]) != 0){
-                if(maxLength < currLength){
-                    maxLength = currLength;
-                }
-                l = tempHash[s[r]] + 1;
-                currLength = currLength - l + 1;
-                tempHash.clear();
-                for(int i = l; i <= r; i++) tempHash[s[i]] = i;
-            }else{
-                tempHash[s[r]] = r;
-                currLength++;
+            cout << l << " " << r << " " << currLength << endl;
+            if(maxLength < currLength){
+                maxLength = currLength;
             }
+            if(tempHash.count(s[r]) != 0){
+                cout << "problem: " << tempHash[s[r]] << endl;
+                    currLength = r - tempHash[s[r]];
+                while(l <= tempHash[s[r]]){
+                    tempHash.erase(s[l]);
+                    l++;
+                }
+            }else{
+                currLength++;
+                if(maxLength < currLength){
+                maxLength = currLength;
+                }
+            }
+            tempHash[s[r]] = r;
             r++;
+
         }
         return maxLength;
     }
@@ -30,6 +37,6 @@ public:
 int main()
 {
     Solution sol;
-    string s = "abcabcbb";
+    string s = " ";
     cout << sol.lengthOfLongestSubstring(s);
 }
