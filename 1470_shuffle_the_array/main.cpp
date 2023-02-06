998e6592e7e4d0b7d77fbff28c23bc9c2d68e5b3
Ervin <75392265+FrequentErvin@users.noreply.github.com>
2023-02-06 22:48:18 +0100

    shuffle the array, maximum subarray sum

--- 1470_shuffle_the_array/main.cpp
+++ 1470_shuffle_the_array/main.cpp
@@ -4,17 +4,24 @@ using namespace std;
 class Solution {
 public:
     vector<int> shuffle(vector<int>& nums, int n) {
-        vector<int> ans;
         for(int i = 0; i < n; i++){
-            ans[i]
+            nums[i] = nums[i]<<10;
+            nums[i] = nums[i]|nums[n+i];
         }
-        return ans;
+        /*for(int i = 0; i < n; i++){
+            cout << nums[i] << endl;
+        }*/
+        for(int i = 0; i < n; i++){
+            nums[2*n-1-2*i] = nums[n-1-i]&(1023);
+            nums[2*n-2-2*i] = nums[n-1-i]>>10;
+        }
+        return nums;
     }
 };
 int main()
 {
     vector<int> etwa{1,4,5,6,7,12,32,30};
     Solution sol;
-    vector<int> ans = sol.shuffle(etwa,4);
-    for(auto x : ans) cout << x << endl;
+    vector<int> nums = sol.shuffle(etwa,4);
+    for(auto x : nums) cout << x << endl;
 }
